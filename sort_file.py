8b33025845cfb1fd00986d89bd83100e144a47a7
Adam James-Liebau Johnson <riizade@gmail.com>
2015-07-17 16:21:32 -0700

    wrote a sorting utility to ease git use with the cube

---
+++ sort_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+__author__ = 'riizade@gmail.com'
+
+import sys
+inputfile = sys.argv[1]
+outputfile = sys.argv[1]
+if len(sys.argv) == 3:
+	outputfile = sys.argv[2]
+
+infile = open(inputfile, "r")
+
+sorted_lines = sorted(infile, key=str.lower)
+infile.close()
+
+outfile = open(outputfile, "w")
+for line in sorted_lines:
+    outfile.write(line)
+
+outfile.close()
